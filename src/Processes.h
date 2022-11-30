168493c696642918c499e82b0a433a93530c5648
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2022-11-30 19:50:35 +0100

    .

--- src/Processes.h
+++ src/Processes.h
@@ -8,10 +8,12 @@
 #include "Program.h"
 #include "simlib.h"
 
+Store worker_capacity = Store("Worker capacity", 5);  // default
+
 class WorkShiftProcess : public Process {
-   public:  // NOLINT
+   public:
     explicit WorkShiftProcess(Program &_program);
-    ~WorkShiftProcess() override = default;
+    ~WorkShiftProcess() override;
 
     void Behavior() override;
 
@@ -20,38 +22,75 @@ class WorkShiftProcess : public Process {
 };
 
 class FermentationProcess : public Process {
-   public:  // NOLINT
+   public:
     explicit FermentationProcess(Program &_program);
     ~FermentationProcess() override;
     void Behavior() override;
 
-   private:  // NOLINT
+   private:
+    //
     Stat *fermentation_duration;
+    Store fermentation_capacity = Store("Fermentation capacity", 1 * 8);  // default (1 fermentation room * 8 carts)
+
+    //
     Program program;
 };
 
 class BakeProcess : public Process {
-   public:  // NOLINT
-    BakeProcess(const double _bake_duration_mean, const double _bake_duration_deviation)
-        : bake_duration_mean_sec(_bake_duration_mean), bake_duration_deviation_sec(_bake_duration_deviation) {}
-    ~BakeProcess() override = default;
+   public:
+    explicit BakeProcess(Program &_program);
+    ~BakeProcess() override;
     void Behavior() override;
 
-   private:  // NOLINT
+   private:
+    //
     Stat *bake_duration{};
+    Store bake_capacity = Store("Bake capacity", 1 * 8);  // default (1 oven * 8 carts)
 
-    const double bake_duration_mean_sec;
-    const double bake_duration_deviation_sec;
+    // Data
+    const double bake_duration_mean_sec = 10 * SECONDS_PER_MINUTE;
+    const double bake_duration_deviation_sec = 2 * SECONDS_PER_MINUTE;
+
+    //
+    Program program;
+};
+
+class CutProcess : public Process {
+   public:
+    explicit CutProcess(Program &_program);
+    ~CutProcess() override;
+    void Behavior() override;
+
+   private:
+    //
+    Stat *cutting_duration{};
+    Store cutting_capacity = Store("Cutting capacity", 1);  // default (only 1 person can make loaves of bread)
+
+    // Data
+    const double cut_duration_mean_per_cart_sec = 10 * SECONDS_PER_MINUTE;
+    const double cut_duration_deviation_per_cart_sec = 2 * SECONDS_PER_MINUTE;
+
+    //
+    Program program;
 };
 
 class MixProcess : public Process {
-   public:  // NOLINT
-    MixProcess() = default;
-    ~MixProcess() override = default;
+   public:
+    explicit MixProcess(Program &_program);
+    ~MixProcess() override;
     void Behavior() override;
 
-   private:  // NOLINT
+   private:
+    //
     Stat *mix_duration;
+    Store mixer_capacity = Store("Mixer capacity", 1);  // default (1 mixer available)
+
+    // Data
+    const double mix_duration_mean_per_cart_sec = 10 * SECONDS_PER_MINUTE;
+    const double mix_duration_deviation_per_cart_sec = 4 * SECONDS_PER_MINUTE;
+
+    //
+    Program program;
 };
 
 #endif  // SRC_PROCESSES_H_
